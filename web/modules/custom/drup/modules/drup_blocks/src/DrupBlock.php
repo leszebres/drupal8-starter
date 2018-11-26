174399b8b15e7a7164513a5daa2727bf83640608
Mathieu MAINGRET <mathieu@leszebres.fr>
2018-11-26 17:41:59 +0100

    DrupBlock format review

--- web/modules/custom/drup/modules/drup_blocks/src/DrupBlock.php
+++ web/modules/custom/drup/modules/drup_blocks/src/DrupBlock.php
@@ -17,25 +17,31 @@ abstract class DrupBlock {
         $options = array_merge([
             'type' => 'blocks'
         ], $options);
-        
+
         $themePath = '/' . drupal_get_path('theme', 'drup_theme');
         $themePathBlocks = $themePath . '/templates/' . $options['type'];
-        
+
         foreach ($themes as $themeID => &$theme) {
+            $template = null;
+
             if (strpos($themeID, 'drup_' . $options['type']) !== false) {
-                
                 // Admin
                 if (strpos($themeID, 'drup_' . $options['type'] . '_admin') !== false) {
                     $template = str_replace('drup_' . $options['type'] . '_admin_', '', $themeID);
                     $theme['variables']['admin_url'] = null;
-                } else {
+                }
+                else {
                     $template = str_replace('drup_' . $options['type'] . '_', '', $themeID);
                 }
-                
-                $theme['path'] = $themePathBlocks;
-                $theme['template'] = str_replace('_', '-', strtolower($template));
-                $theme['variables']['theme_path'] = $themePath;
             }
+
+            if ($template === null) {
+                $template = str_replace('drup_' , '', $themeID);
+            }
+
+            $theme['path'] = $themePathBlocks;
+            $theme['template'] = str_replace('_', '-', strtolower($template));
+            $theme['variables']['theme_path'] = $themePath;
         }
     }
     
