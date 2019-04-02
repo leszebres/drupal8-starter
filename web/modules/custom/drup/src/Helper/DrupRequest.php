ef5f1452864e3a5531ac943d226f7a8e4435cdf7
Mathieu MAINGRET <mathieu@leszebres.fr>
2019-04-02 15:41:41 +0200

    Fix 404 message

--- web/modules/custom/drup/src/Helper/DrupRequest.php
+++ web/modules/custom/drup/src/Helper/DrupRequest.php
@@ -40,7 +40,7 @@ abstract class DrupRequest {
 
         $content404 = '<h2 class="title--h3">' . t('You may have followed a broken link, or tried to view a page that no longer exists.') . '</h2>';
         if ($contact = $drupRouter->getPath('contact')) {
-            $content404 .= '<p>' . t('If the problem persists, <a href="%link">contact us</a>.', ['%link' => $contact]) . '</p>';
+            $content404 .= '<p>' . t('If the problem persists, <a href=":link">contact us</a>.', [':link' => $contact]) . '</p>';
         }
         $content404 .= '<p><a href="' . Url::fromRoute('<front>')->toString() . '" class="btn btn--primary">' . t('Back to the front page') . '</a></p>';
 
