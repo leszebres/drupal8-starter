ff488d92092f65ab735ea0d61ccbe62138d4e69c
Mathieu MAINGRET <mathieu@leszebres.fr>
2019-04-24 13:33:37 +0200

    Twig filter for phone numbers

---
+++ web/modules/custom/drup/src/TwigExtension/PhoneNumber.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\drup\TwigExtension;
+
+/**
+ * Class PhoneNumber
+ *
+ * Sanitize a phone number for link src attribute with tel: prefix
+ *
+ * @example <a href="{{ '05 55 55 55 55'|phone_number }}"></a> => <a href="tel:0555555555"></a>
+ *
+ * @package Drupal\drup\TwigExtension
+ */
+class PhoneNumber extends \Twig_Extension {
+
+    /**
+     * @return array|\Twig\TwigFilter[]
+     */
+    public function getFilters() {
+        return [new \Twig_SimpleFilter('phone_number', [$this, 'phoneNumber'])];
+    }
+
+    /**
+     * @param $string
+     *
+     * @return string
+     */
+    public static function phoneNumber($string) {
+        $string = str_replace([' ', '.'], '', $string);
+        $string = 'tel:' . $string;
+        return $string;
+    }
+}
