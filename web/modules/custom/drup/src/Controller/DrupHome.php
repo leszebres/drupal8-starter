1ab914cd47318f9a616fbd4ad1234b10c5b989cb
Kévin Baubet <kevin@leszebres.fr>
2019-05-06 16:13:16 +0200

    Add custom homepage

---
+++ web/modules/custom/drup/src/Controller/DrupHome.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\drup\Controller;
+
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class DrupHome
+ *
+ * @package Drupal\drup\Controller
+ */
+class DrupHome extends ControllerBase {
+    public function build() {
+        return [];
+    }
+}
