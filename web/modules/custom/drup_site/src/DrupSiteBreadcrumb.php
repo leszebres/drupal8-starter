20dfe01b86e7326d2a5bb0fac5548598277222ae
Kévin Baubet <kevin@leszebres.fr>
2019-08-07 14:06:12 -0400

    #fix breacrumb examples

--- web/modules/custom/drup_site/src/DrupSiteBreadcrumb.php
+++ web/modules/custom/drup_site/src/DrupSiteBreadcrumb.php
@@ -18,28 +18,28 @@ class DrupSiteBreadcrumb extends DrupBreadcrumb {
      */
     public function buildList() {
         $breadcrumbs = [
-            'drup_route' => [
-                // contact => []
-            ],
-            // ENTITY_TYPE (ex : taxonomy_term, node) | DRUP ROUTE | SYSTEM ROUTE => [
-            //     BUNDLE (ex : node_type or vocabulary) | DRUP ROUTE NAME | SYSTEM ROUTE NAME => [
-            //            ID (ex : DrupRouteName, Field id without field_ prefix) => TYPE (drup_route/referenced_entity/referenced_taxonomy_term_parents),
-            //            ...
-            //     ], ...
-            //]
-            //
-            'node' => [
-                'page' => [
-                    //'contact' => 'drup_route',
-                    //'nodes' => 'referenced_entity',
-                    //'terms' => 'referenced_taxonomy_term_parents',
-                ]
-            ],
-            'taxonomy_term' => [
-            ],
-            'system' => [
-                // drup_site.home => []
-            ]
+//            'drup_route' => [
+//                 'contact' => []
+//            ],
+//             ENTITY_TYPE (ex : taxonomy_term, node) | DRUP ROUTE | SYSTEM ROUTE => [
+//                 BUNDLE (ex : node_type or vocabulary) | DRUP ROUTE NAME | SYSTEM ROUTE NAME => [
+//                        ID (ex : DrupRouteName, Field id without field_ prefix) => TYPE (drup_route/referenced_entity/referenced_taxonomy_term_parents),
+//                        ...
+//                 ], ...
+//            ]
+//
+//            'node' => [
+//                'page' => [
+//                    'contact' => 'drup_route',
+//                    'nodes' => 'referenced_entity',
+//                    'terms' => 'referenced_taxonomy_term_parents',
+//                ]
+//            ],
+//            'taxonomy_term' => [
+//            ],
+//            'system' => [
+//                 'drup_site.home' => []
+//            ]
         ];
 
         return $breadcrumbs;
