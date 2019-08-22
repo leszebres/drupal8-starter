6449d4e7f315f0db9cb7706e6e5337638f40392f
Mathieu MAINGRET <mathieu@leszebres.fr>
2019-08-22 11:45:56 +0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -25,15 +25,16 @@ set('default_stage', 'draft');
 set('drupal_site', 'default');
 
 add('shared_files', [
-    'web/sites/{{drupal_site}}/settings.php',
-    'web/sites/{{drupal_site}}/services.yml'
+    'web/.htaccess',
+    'web/sites/{{drupal_site}}/settings.php'
 ]);
 add('shared_dirs', [
-    'web/sites/{{drupal_site}}/files'
+    'web/sites/{{drupal_site}}/files',
+    'web/themes/custom/drup_theme/node_modules',
 ]);
 
 add('writable_dirs', [
-    'web/sites/{{drupal_site}}/files'
+    //'web/sites/{{drupal_site}}/files'
 ]);
 
 
@@ -65,25 +66,31 @@ task('drush:langimp', function () {
     run('php {{release_path}}/vendor/bin/drush langimp {{release_path}}/web/themes/custom/drup_theme/translations/drup_theme-8.x-1.0.fr.po');
 });
 
+desc('Load gulp dependencies');
+task('deploy:node_modules', function () {
+    run('cd {{release_path}}/web/themes/custom/drup_theme && yarn --production=true');
+});
+
 desc('Update database');
 task('drush:updb', function () {
     run('php {{release_path}}/vendor/bin/drush updb -y');
 });
 
-desc('Reload features');
-task('drush:fra', function () {
-    run('php {{release_path}}/vendor/bin/drush fra -y');
+desc('Revert feature_content');
+task('drush:fr', function () {
+    run('php {{release_path}}/vendor/bin/drush fr feature_content -y');
 });
 
 
-after('deploy:symlink', 'drush:updb');
+after('deploy:symlink', 'drush:fr');
+after('drush:fr', 'drush:updb');
 after('drush:updb', 'drush:langimp');
-//after('drush:updb', 'drush:fra');
 after('drush:langimp', 'drush:cr');
 
 
 // Composer dependencies
 after('deploy:shared', 'deploy:vendors');
+after('deploy:vendors', 'deploy:node_modules');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
